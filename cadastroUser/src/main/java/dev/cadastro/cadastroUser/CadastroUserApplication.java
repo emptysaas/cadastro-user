768dda1c1688d13f3a3e7440d3a4c4d68d2048b2
emptysaas <luiz.gus.ms@gmail.com>
2025-05-20 18:29:11 -0300

    first commit

---
+++ cadastroUser/src/main/java/dev/cadastro/cadastroUser/CadastroUserApplication.java
@@ -0,0 +1,13 @@
+package dev.cadastro.cadastroUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroUserApplication.class, args);
+	}
+
+}
---
+++ cadastroUser/src/test/java/dev/cadastro/cadastroUser/CadastroUserApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cadastro.cadastroUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
